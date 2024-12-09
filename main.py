17c17faa447d8e6d4a0535dd26a30f20c0783b14
TrvDylan <112314634+TrvDylan@users.noreply.github.com>
2024-12-09 10:59:39 +0100

    Test print/log

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from fastapi import FastAPI
+import sys
 import logging
 
-logging.info("Test log")
+# Logs
+logging.basicConfig(level=logging.INFO)
+sys.stdout = sys.stderr
+
+print("print : test avant l'app")
+logging.info("log : test avant l'app")
 
 app = FastAPI()
 
+print("print : test apres l'app")
+logging.info("log : test apres l'app")
+
 @app.get("/")
 def read_root():
-    print("Before Get")
-    logging.info("Test avant le return")
+    print("print : test pendant le get")
+    logging.info("log : test pendant le get")
     return {"message": "Hello World!"}
-    logging.info("Test après le return")
