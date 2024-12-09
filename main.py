b590e876a7d01588bde5146c38e891320653857f
TrvDylan <112314634+TrvDylan@users.noreply.github.com>
2024-12-09 11:08:19 +0100

    Chargement du model et du scaler

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 from fastapi import FastAPI
 import sys
 import logging
+import joblib
 
 # Logs
 logging.basicConfig(level=logging.INFO)
 sys.stdout = sys.stderr
 
-print("print : test avant l'app")
-logging.info("log : test avant l'app")
+# Variables
+model_path = "model.pkl"
+scaler_path = "scaler.pkl"
 
+# Création de l'API
 app = FastAPI()
 
-print("print : test apres l'app")
-logging.info("log : test apres l'app")
+# Chargement du modele de ML et du scaler avec joblib
+try:
+    model = joblib.load(model_path)
+    scaler = joblib.load(scaler_path)
+except Exception as e:
+    print(f"Erreur lors du chargement des fichiers : {e}")
+
+print("model : ", model)
+print("scaler : ", scaler)
 
 @app.get("/")
 def read_root():
